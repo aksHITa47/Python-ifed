655d4a90f5b3e650238f00db779917f9f5f246d8
Akshita <akshitasingh2411@gmail.com>
2021-01-10 01:02:42 +0530

    step 4 completed

--- step_4.py
+++ step_4.py
@@ -3,19 +3,31 @@ import numpy as np
 def intersection_numpy(list1,list2):
     """Find the overalapping values of two lists.
 
+    Makes use of the NumPy library to find the intersection of 
+    the lists. 
+
     Parameters
     ----------
     list1 (array_like) : First list 
-    list2 (array_like) : SEcond list
+    list2 (array_like) : Second list
 
     Returns
     -------
     A numpy.ndarray datatype containing the common elements of the lists
+
+    Note
+    ----
+    NumPy is not a part of the Python Standard Library,so
+    it must be installed separately using Pip before using it in your code.
+    Deatiled instrucions for installing NumPy can be found at: https://numpy.org/install/
     """
     return(np.intersect1d(list1, list2, assume_unique=False))
 
 def intersection_datatype(list1, list2):
     """Find the overlapping values of two lists.
+
+    Makes use of Python's datastructure, sets to find intersection of the 
+    lists.
 
     Parameters
     ----------
